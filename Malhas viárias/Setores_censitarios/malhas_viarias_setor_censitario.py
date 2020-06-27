52dd1f2e8d69e3826ba5832f30fac5e0261f5e89
Rafael Santos <>
2020-06-27 18:13:34 -0300

    malhas viarias setor censitário

--- Malhas viárias/Setores_censitarios/malhas_viarias_setor_censitario.py
+++ Malhas viárias/Setores_censitarios/malhas_viarias_setor_censitario.py
@@ -1,3 +1,13 @@
 from geobr import read_census_tract
-censi = read_census_tract(code_tract='all', year=2010)
-print(censi)
+
+# regiões censitárias do Brasil
+censiBR = read_census_tract(code_tract='all', year=2010)
+print(censiBR)
+
+# regiões censitárias em Minas Gerais
+censiMG = read_census_tract(code_tract='MG', year=2010)
+print(censiMG)
+
+# regiões censitárias de BH
+censiBH = read_census_tract(code_tract=3106200, year=2010)
+print(censiBH)
