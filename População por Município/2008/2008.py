38d5a2f2095b4c6a19f8ef53611255b2ca94062a
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2020-07-04 17:40:57 -0300

    Add files via upload

---
+++ População por Município/2008/2008.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib
+
+pop08 = pd.read_csv("2008.csv",sep = ";")
+pop08["GEOCODIG_M"] = pop08["COD_UF"]*100000 + pop08["COD_MN"]
+
+br08 = gpd.read_file("shp/55mu2500psd.shp")
+
+mapa = br08.merge(pop08, on = "GEOCODIG_M")
+
+#Ver  tabela
+#print(mapa)
+
+#Plotar o mapa
+#mapa.plot(column = "POP_ESTIM", figsize = (20,20), legend = True, legend_kwds={'label': "População Estimada em 2008", 'orientation': "horizontal"})
