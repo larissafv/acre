e9bc0bbdb413e6505f000743ae39f7960c4119bb
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2020-07-04 17:46:13 -0300

    Add files via upload

---
+++ População por Município/2009/2009.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib
+
+pop09 = pd.read_csv("2009.csv",sep = ";")
+pop09["GEOCODIG_M"] = pop09["COD_UF"]*100000 + pop09["COD_MN"]
+
+br09 = gpd.read_file("shp/55mu2500psd.shp")
+
+mapa = br09.merge(pop09, on = "GEOCODIG_M")
+
+#Ver  tabela
+#print(mapa)
+
+#Plotar o mapa
+#mapa.plot(column = "POP_ESTIM", figsize = (20,20), legend = True, legend_kwds={'label': "População Estimada em 2009", 'orientation': "horizontal"})
